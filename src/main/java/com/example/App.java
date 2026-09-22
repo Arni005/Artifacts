976902fdc0c7c67b81c195bda14da11a383c7050
Arni005 <arnijohry@gmail.com>
2026-09-22 15:31:04 +0530

    Initial commit: Maven project structure

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,9 @@
+// src/main/java/com/example/App.java
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from the build artifact demo!");
+        System.out.println("App version: 1.0.0");
+    }
+}
